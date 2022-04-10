40bdbbf22c61a7d16d5e625f6ea5130009d315a2
Anderson Silva <andersondez290@gmail.com>
2022-04-10 12:18:40 -0300

    finalizado o sistema de drop, iniciando o sistema de plantação

--- csa jogo curso/Assets/Scripts/Craft/Tree.cs
+++ csa jogo curso/Assets/Scripts/Craft/Tree.cs
@@ -7,21 +7,38 @@ public class Tree : MonoBehaviour
     [SerializeField] private float treeHealth;
     [SerializeField] private Animator anim;
 
+    [SerializeField] private GameObject woodPrefab;
+    [SerializeField] private int totalWood;
+    [SerializeField] private ParticleSystem leafs;
+
+    private bool isCut;
+
+    private void Start()
+    {
+        totalWood = Random.Range(1, 3);
+    }
+
     public void OnHit()
     {
         treeHealth--;
 
         anim.SetTrigger("isHit");
+        leafs.Play(); 
 
         if (treeHealth <= 0)
         {
+            for (int i = 0; i < totalWood; i++)
+            {
+                Instantiate(woodPrefab, transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f), transform.rotation);
+            }
             anim.SetTrigger("cut");
+            isCut = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Axe"))
+        if (collision.CompareTag("Axe") && !isCut)
         {
             OnHit();
         }
---
+++ csa jogo curso/Assets/Scripts/Player/PlayerItens.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItens : MonoBehaviour
+{
+    public int totalWood;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
