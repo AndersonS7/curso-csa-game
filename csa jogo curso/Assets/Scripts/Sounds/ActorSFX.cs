787bd5af540c6aa54a05fada3840eb953a22a662
Anderson Silva <andersondez290@gmail.com>
2022-04-17 21:15:37 -0300

    testando sistema de criação de terreno

--- csa jogo curso/Assets/Scripts/Sounds/ActorSFX.cs
+++ csa jogo curso/Assets/Scripts/Sounds/ActorSFX.cs
@@ -6,6 +6,11 @@ public class ActorSFX : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
 
+    private void Start()
+    {
+        audioSource.volume = 0.2f;
+    }
+
     public void PlaySFX(AudioClip clip)
     {
         audioSource.PlayOneShot(clip);
